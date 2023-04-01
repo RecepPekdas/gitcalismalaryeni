4b182eb52d4c6c6ffd4dbf00cb169cf726964838
AlperVatansever <alpervat@gmail.com>
2023-04-01 11:19:09 +0300

    feat:add user inputs

--- Program.cs
+++ Program.cs
@@ -3,8 +3,14 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            //Kim Milyoner Olmak İster??
             Console.WriteLine("Kim Milyoner Olmak İster\n");
+            Console.WriteLine("Kim Milyoner Olmak İster\n");
+            Console.Write("İsim Giriniz: ");
+            string isim = Console.ReadLine();
+            Console.Write("Soyisim Giriniz: ");
+            string soyisim = Console.ReadLine();
         }
     }
 }
