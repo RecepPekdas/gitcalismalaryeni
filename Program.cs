dd0101b1661d90c8fa109ee1d1ebea4666c897a7
AlperVatansever <alpervat@gmail.com>
2023-04-01 11:28:32 +0300

    feat: add if blocks

--- Program.cs
+++ Program.cs
@@ -22,6 +22,21 @@
 
             Console.Write("Kuralları Biliyor Musunuz? : ");
             char kuralCevap = char.Parse(Console.ReadLine().ToUpper());
+            if (kuralCevap != 'E')
+            {
+                // TODO: Kurallar eklenecek.
+                Console.WriteLine("Kurallar: asdfasdaads");
+            }
+
+            Console.Write("Hazır Mısınız? : ");
+            char hazirCevap = char.Parse(Console.ReadLine().ToUpper());
+
+            if (hazirCevap != 'E')
+            {
+                Console.WriteLine("Hazır olmananız bekleniyor.");
+                Console.WriteLine("Hazır olduğunuzda Enter'a Basın");
+                Console.ReadLine();
+            }
         }
     }
 }
